b263e6b3edc4f3a1e5b0aca3f0659365780bebb5
Bukk94 <st46557@student.upce.cz>
2017-07-08 23:33:00 +0200

    Testing MainFrame form

---
+++ Medbot/MainFrame.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Medbot {
+    public partial class MainFrame : Form {
+        
+        /// <summary>
+        /// Test interface for API
+        /// </summary>
+        public MainFrame() {
+            InitializeComponent();
+
+            IBotClient bot = new BotClient(this);
+            Thread thread = new Thread(new ThreadStart(bot.Start));
+            thread.Start();
+        }
+
+        public delegate void UpdateTextBoxDelegate(string text);
+        public void ConsoleAppendText(string text) {
+            if (textBox.InvokeRequired) {
+                textBox.BeginInvoke(new UpdateTextBoxDelegate(ConsoleAppendText), new Object[] { text });
+            } else {
+                textBox.AppendText(Environment.NewLine + text);
+                textBox.ScrollToCaret();
+            }
+        }
+    }
+}
