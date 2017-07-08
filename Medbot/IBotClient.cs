e323800b6220fd44c45dad900057f59b98e548eb
Bukk94 <st46557@student.upce.cz>
2017-07-08 23:33:07 +0200

    Bot client interface
    Bot client interface

---
+++ Medbot/IBotClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medbot {
+    interface IBotClient {
+        void Start();
+        void Connect();
+        User GetUserFromChat(string chatLine);
+        void SendChatMessage(string msg);
+        void SendPrivateMessage(string msg, string user);
+
+    }
+}
