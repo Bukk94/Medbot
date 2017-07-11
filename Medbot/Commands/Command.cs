42047f90aa3d0ece44b9a38e36039884d3076486
Bukk94 <st46557@student.upce.cz>
2017-07-12 01:23:24 +0200

    Added more command handlers, move to folder
    Added more command handlers, move to folder
    Added internal type

---
+++ Medbot/Commands/Command.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Medbot.Commands {
+    enum CommandType { Points, XP, Internal }
+    enum HandlerType { Info, Add, Remove, Leaderboard, LastFollower, Random }
+
+    internal class Command {
+        private CommandType commandType;
+        private HandlerType handlerType;
+        private string commandFormat;
+        private string aboutCommand;
+        private string successMessage;
+        private string failMessage;
+        private bool broadcasterOnly;
+        private bool modRequired;
+        private bool sendWhisper;
+
+        /// <summary>
+        /// Gets an command format e.g !points
+        /// </summary>
+        internal string CommandFormat { get { return this.commandFormat; } }
+
+        /// <summary>
+        /// Gets an command handler type (e.g. Add, Remove)
+        /// </summary>
+        internal HandlerType CommandHandlerType { get { return this.handlerType; } }
+
+        /// <summary>
+        /// Gets a boolean if the command can be executed only by broadcaster
+        /// </summary>
+        internal bool BroadcasterOnly { get { return this.broadcasterOnly; } }
+
+        /// <summary>
+        /// Gets a boolean if the moderator permissions are required to use the command
+        /// </summary>
+        internal bool ModeratorPermissionRequired { get { return this.modRequired; } }
+
+        /// <summary>
+        /// Gets command's fail message 
+        /// </summary>
+        internal string FailMessage { get { return this.failMessage; } }
+
+        /// <summary>
+        /// Gets command's success message
+        /// </summary>
+        internal string SuccessMessage { get { return this.successMessage; } }
+
+        /// <summary>
+        /// Gets bool if bot should send command's result as whisper
+        /// </summary>
+        internal bool SendWhisper { get { return this.sendWhisper; } }
+
+        internal Command(CommandType cmd, HandlerType handler, string commandFormat, string about, string successMessage, 
+                         string failMessage, bool broadcasterOnly, bool modPermission, bool sendWhisp) {
+            this.handlerType = handler;
+            this.commandFormat = commandFormat;
+            this.aboutCommand = about;
+            this.successMessage = successMessage;
+            this.failMessage = failMessage;
+            this.commandType = cmd;
+            this.broadcasterOnly = broadcasterOnly;
+            this.modRequired = modPermission;
+            this.sendWhisper = sendWhisp;
+        }
+
+        /// <summary>
+        /// Executes command
+        /// </summary>
+        /// <param name="sender">User who executed the command</param>
+        /// <param name="command">Full name of the command</param>
+        /// <returns>Informative string</returns>
+        internal string Execute(User sender, List<string> args) {
+            if (CheckCommandPermissions(sender))
+                return CommandsHandler.ExecuteMethod(commandType, this, sender, args);
+            else
+                return String.Format("Nemáš dostatečná práva abys provedl tento příkaz. MedBot ti nepomůže.");
+        }
+
+        /// <summary>
+        /// Checks if user has permission to use the command
+        /// </summary>
+        /// <param name="sender">User who executed the command</param>
+        /// <returns>Bool value if he has permission to use the command</returns>
+        public bool CheckCommandPermissions(User sender) {
+            return (BroadcasterOnly && sender.Broadcaster) || (ModeratorPermissionRequired && (sender.Moderator || sender.Broadcaster) || (!BroadcasterOnly && !ModeratorPermissionRequired));
+        }
+
+        /// <summary>
+        /// Verifies command format. {0} is always a number, {1} is always string
+        /// </summary>
+        /// <param name="chatCommand">String command to check</param>
+        /// <returns>Bool value if command format is correct</returns>
+        internal bool VerifyFormat(string chatCommand) {
+            string cmdFormat = this.CommandFormat.Replace("{0}", @"\d+");
+            cmdFormat = cmdFormat.Replace("{1}", @"\w+");
+            Regex rgx = new Regex("^" + cmdFormat +"$", RegexOptions.IgnoreCase);
+
+            return rgx.IsMatch(chatCommand);
+        }
+
+        /// <summary>
+        /// Returns command's info message as help
+        /// </summary>
+        /// <returns>String message</returns>
+        internal string GetAboutInfoMessage() {
+            // 3 params {0:Currency name} {1:plural} {2: units}
+            return String.Format(aboutCommand, Points.CurrencyName, Points.CurrencyNamePlural, Points.CurrencyUnits);
+        }
+    }
+}
---
+++ Medbot/Commands/CommandsHandler.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Medbot.LoggingNS;
+using Medbot.Followers;
+
+namespace Medbot.Commands {
+    internal static class CommandsHandler {
+        private static string commandsFilePath = Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar + "Commands.xml";
+        private static Points pointsObject;
+        // TODO: !leaderboard (top 3 xp, top 3 pointu)
+        // TODO: !leaderboard med/xp (vypise kdo ma aktualne nejvic medu)
+        // TODO: !tradehoney {0: pocet} {1:target}
+
+        /// <summary>
+        /// Initializes Commands Handler, passing points and XP objects
+        /// </summary>
+        /// <param name="points">Object of Points class</param>
+        // TODO: XP reference
+        internal static void Initialize(Points points) {
+            pointsObject = points;
+        }
+
+        internal static List<Command> LoadCommands() {
+            if (!File.Exists(commandsFilePath)) {
+                Logging.LogError(typeof(CommandsHandler), System.Reflection.MethodBase.GetCurrentMethod(), "FAILED to load commands. File not found");
+                return null;
+            }
+
+            List<Command> commandsList = new List<Command>();
+            try {
+                XDocument data = XDocument.Load(commandsFilePath);
+                var commandsTypeGroups = data.Element("Medbot").Elements("Commands");
+
+                foreach (var cmdGroup in commandsTypeGroups) {
+                    CommandType cmdType = (CommandType)Enum.Parse(typeof(CommandType), cmdGroup.Attribute("Type").Value);
+                    var commands = cmdGroup.Elements("Command");
+
+                    foreach (var cmd in commands) {
+                        HandlerType handler; 
+                        if(!Enum.TryParse<HandlerType>(cmd.Attribute("Handler").Value, out handler))
+                            continue;
+
+                        bool broadcasterOnly = Parsing.ParseBooleanFromAttribute(cmd, "BroadcasterOnly");
+
+                        Command newCmd = new Command(cmdType, handler, cmd.Value.ToLower(), 
+                                                     cmd.Attribute("AboutCommand").Value, 
+                                                     cmd.Attribute("SuccessMessage").Value, 
+                                                     cmd.Attribute("FailMessage").Value,
+                                                     broadcasterOnly,
+                                                     broadcasterOnly ? false : Parsing.ParseBooleanFromAttribute(cmd, "ModPermissionRequired"),
+                                                     Parsing.ParseBooleanFromAttribute(cmd, "SendWhisper")
+                                                     );
+                        commandsList.Add(newCmd);
+                    }
+                }
+
+                Logging.LogEvent(System.Reflection.MethodBase.GetCurrentMethod(), "Commands Loaded");
+            } catch (Exception ex) {
+                Logging.LogError(typeof(CommandsHandler), System.Reflection.MethodBase.GetCurrentMethod(), ex.ToString());
+                return null;
+            }
+
+            return commandsList;
+        }
+
+        /// <summary>
+        /// Decides which handler should operate the command
+        /// </summary>
+        /// <param name="methodType">Method type</param>
+        /// <param name="cmd">Command to execute</param>
+        /// <param name="sender">User who sent the command</param>
+        /// <param name="args">Command arguments</param>
+        /// <returns>String result</returns>
+        public static string ExecuteMethod(CommandType methodType, Command cmd, User sender, List<string> args) {
+            string result = String.Empty;
+
+            switch (methodType) {
+                case CommandType.Points:
+                    result = PointsHandler(sender, cmd, args);
+                    break;
+                case CommandType.XP:
+                    result = ExperienceHandler(sender, cmd, args);
+                    break;
+                case CommandType.Internal:
+                    result = InternalHandler(sender, cmd, args);
+                    break;
+                default:
+                    result = "";
+                    break;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Method handling Points command distribution
+        /// </summary>
+        /// <param name="sender">User who sent the command</param>
+        /// <param name="cmd">Command to execute</param>
+        /// <param name="args">Command arguments</param>
+        /// <returns>String result</returns>
+        private static string PointsHandler(User sender, Command cmd, List<string> args) {
+            switch (cmd.CommandHandlerType) {
+                case HandlerType.Add:
+                    // Add points,  !addhoney 50 Bukk94 |  2 input args {0:Number of points} {1:User to which add points}
+                    
+                    User receiver = BotClient.FindOnlineUser(args[1]);
+                    try {
+                        if (receiver == null) { // user is not online
+                            pointsObject.AddUserPointsToFile(args[1], long.Parse(args[0]));
+                        } else {
+                            receiver.AddPoints(long.Parse(args[0]));
+                            pointsObject.SavePoints();
+                        }
+                        Logging.LogEvent(System.Reflection.MethodBase.GetCurrentMethod(), 
+                                                 String.Format("{0}  Args: {1}, {2} - Points successfully added", cmd.CommandFormat, args[0], args[1]));
+
+                        // Success: 4 params: {0:User} {1:Number of points} {2:Currency plural} {3:Currency units}
+                        return String.Format(cmd.SuccessMessage, args[1], args[0], Points.CurrencyNamePlural, Points.CurrencyUnits);
+                    } catch (Exception ex) {
+                        Logging.LogError(typeof(CommandsHandler), System.Reflection.MethodBase.GetCurrentMethod(), ex.ToString());
+
+                        // Fail: 5 params: {0:Currency name} {1:Currency plural} {2:Currency units} {3:Number of points} {4:User to which add points} 
+                        return String.Format(cmd.FailMessage, Points.CurrencyName, Points.CurrencyNamePlural, Points.CurrencyUnits, args[0], receiver != null ? receiver.DisplayName : args[1]);
+                    }
+
+                case HandlerType.Remove:
+                    //_Remove points, !removehoney 50 Bukk94 |  2 input args {0:Number of points} {1:User to which remove points}
+                    User targetUser = BotClient.FindOnlineUser(args[1]);
+                    try {
+                        if (targetUser == null) { // user is not online
+                            pointsObject.RemoveUserPointsFromFile(args[1], long.Parse(args[0]));
+                        } else {
+                            targetUser.RemovePoints(long.Parse(args[0]));
+                            pointsObject.SavePoints();
+                        }
+
+                        Logging.LogEvent(System.Reflection.MethodBase.GetCurrentMethod(), 
+                                                 String.Format("{0}  Args: {1}, {2} - Points successfully removed", cmd.CommandFormat, args[0], args[1]));
+
+                        // Success: 4 params: {0:User} {1:Number of points} {2:Currency plural} {3:Currency units}
+                        return String.Format(cmd.SuccessMessage, args[1], args[0], Points.CurrencyNamePlural, Points.CurrencyUnits);
+                    } catch (Exception ex) {
+                        Logging.LogError(typeof(CommandsHandler), System.Reflection.MethodBase.GetCurrentMethod(), ex.ToString());
+
+                        // Fail: 5 params: {0:Currency name} {1:Currency plural} {2:Currency units} {3:Number of points} {4:User to which remove points} 
+                        return String.Format(cmd.FailMessage, Points.CurrencyName, Points.CurrencyNamePlural, Points.CurrencyUnits, args[0], targetUser != null ? targetUser.DisplayName : args[1]);
+                    }
+
+                case HandlerType.Info:
+                    // Get points info !med |  0 input args
+                    if (sender == null) { // Fail: 5 params: {0:Currency name} {1:Currency plural} {2:Currency units} {3:Number of points} {4:User to which add points}
+                        Logging.LogError(typeof(CommandsHandler), System.Reflection.MethodBase.GetCurrentMethod(), String.Format("{0} - Sender is null", cmd.CommandFormat));
+                        return String.Format(cmd.FailMessage, Points.CurrencyName, Points.CurrencyNamePlural, Points.CurrencyUnits, "N/A", "N/A");
+                    }
+
+                    Logging.LogEvent(System.Reflection.MethodBase.GetCurrentMethod(),
+                                                 String.Format("{0}  Args: None # Points printed for user {1} # Value: {2}", cmd.CommandFormat, sender.DisplayName, sender.Points));
+
+                    // Success: 4 params: {0:User} {1:Total} {2:Currency plural} {3:Currency units}
+                    return String.Format(cmd.SuccessMessage, sender.DisplayName, sender.Points, Points.CurrencyNamePlural, Points.CurrencyUnits); 
+            }
+
+            return "Unknown Points handler";
+        }
+
+        private static string ExperienceHandler(User sender, Command cmd, List<string> args) {
+            // TODO: Experience Handler
+            return "";
+        }
+
+        /// <summary>
+        /// Method handling internal command distribution
+        /// </summary>
+        /// <param name="sender">User who sent the command</param>
+        /// <param name="cmd">Command to execute</param>
+        /// <param name="args">Command arguments</param>
+        /// <returns>String result</returns>
+        private static string InternalHandler(User sender, Command cmd, List<string> args) {
+            switch (cmd.CommandHandlerType) {
+                case HandlerType.LastFollower:
+                    // Last follower, !lastfollower |  0 input args
+                    try {
+                        Follow last = FollowersClass.GetNewestFollower(Login.Channel, Login.ClientID).Result;
+                        if (last == null) // Fail: 0 params
+                            throw new Exception("Last follower has not been found");
+
+                        // Success: 3 params: {0:User} {1:Date} {2:Notification (bool)}
+                        return String.Format(cmd.SuccessMessage, last.Follower.DisplayName, last.CreatedAt.ToShortDateString(), last.Notifications.ToString().ToLower());
+                    } catch (Exception ex) {
+                        Logging.LogError(typeof(CommandsHandler), System.Reflection.MethodBase.GetCurrentMethod(), ex.ToString());
+                        return cmd.FailMessage;
+                    }
+
+                case HandlerType.Random:
+                    // Selects random user,  !random   !random {0}  | 0 or 1 input args {0: Active past X minutes}
+                    try {
+                        User randomUser = null;
+                        int totalUsers = 0;
+                        if (args.Count == 0) {
+                            // Regular random
+                            randomUser = SelectRandomUser(BotClient.OnlineUsers);
+                            totalUsers = BotClient.OnlineUsers.Count;
+                        } else {
+                            // Active users random
+                            if (args.Count <= 0)
+                                throw new Exception("Command " + cmd.CommandFormat + "should contain some arguments. Found " + args.Count);
+
+                            List<User> users = BotClient.OnlineUsers.Where(u =>
+                                                            u.LastMessage != null &&
+                                                            DateTime.Now - u.LastMessage < TimeSpan.FromMinutes(int.Parse(args[0]))).ToList();
+                            totalUsers = users.Count;
+                            randomUser = SelectRandomUser(users);
+                        }
+
+                        if (randomUser == null)
+                            throw new Exception("No available users to select a random from.");
+
+                        // Success: 1 param: {0:User} {1:number of people to draw}
+                        return String.Format(cmd.SuccessMessage, randomUser.DisplayName, totalUsers);
+                    } catch (Exception ex) {
+                        Logging.LogError(typeof(CommandsHandler), System.Reflection.MethodBase.GetCurrentMethod(), ex.ToString());
+                        return cmd.FailMessage;
+                    }
+                    break;
+            }
+
+            return "Unknown internal handler";
+        }
+
+        /// <summary>
+        /// Selects random user from the list
+        /// </summary>
+        /// <param name="usersList">List of users</param>
+        /// <returns>Returns random user</returns>
+        private static User SelectRandomUser(List<User> usersList) {
+            if (usersList.Count <= 0)
+                return null;
+            else if (usersList.Count == 1)
+                return usersList[0];
+
+            Random rand = new Random(Guid.NewGuid().GetHashCode());
+            int random = rand.Next(0, usersList.Count);
+            return usersList[random];
+        }
+    }
+}
