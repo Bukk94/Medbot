f2643e0d4fe3ed972d3f2249ccde9905d046f614
Bukk94 <st46557@student.upce.cz>
2017-07-20 01:30:32 +0200

    Custom exception for ranks
    Custom exception for ranks

---
+++ Medbot/Exceptions/RanksException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Medbot.Exceptions {
+    public class RanksException : Exception {
+
+        /// <summary>
+        /// Initializes a new PointsCommndException instance
+        /// </summary>
+        public RanksException() { }
+
+        /// <summary>
+        /// Initializes a new PointsException instance with specific error message
+        /// </summary>
+        /// <param name="message">Message that describes the error</param>
+        public RanksException(string message) : base(message) { }
+
+    }
+}
