b91e54412731e19d4024449618ab84a8d1cb2f9f
Bukk94 <st46557@student.upce.cz>
2017-07-20 01:12:51 +0200

    Changed access modifiers from internal to public
    Changed access modifiers from internal to public

--- Medbot/ExpSystem/Rank.cs
+++ Medbot/ExpSystem/Rank.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Medbot.ExpSystem {
-    internal class Rank {
+    public class Rank {
         private string rankName;
         private long expRequired;
         private int rankLevel;
@@ -13,17 +9,17 @@ namespace Medbot.ExpSystem {
         /// <summary>
         /// Gets a rank name
         /// </summary>
-        internal string RankName { get { return this.rankName; } }
+        public string RankName { get { return this.rankName; } }
 
         /// <summary>
         /// Gets a rank level
         /// </summary>
-        internal int RankLevel { get { return this.rankLevel; } }
+        public int RankLevel { get { return this.rankLevel; } }
 
         /// <summary>
         /// Gets experience required to gain this level
         /// </summary>
-        internal long ExpRequired { get { return this.expRequired; } }
+        public long ExpRequired { get { return this.expRequired; } }
 
         /// <summary>
         /// Rank structure containing rank name, rank level and experience required to gain this rank
@@ -31,7 +27,7 @@ namespace Medbot.ExpSystem {
         /// <param name="name">Name of the rank</param>
         /// <param name="level">Level of the rank</param>
         /// <param name="exp">Experience required to gain rank</param>
-        internal Rank(string name, int level, long exp) {
+        public Rank(string name, int level, long exp) {
             this.rankName = name;
             this.rankLevel = level;
             this.expRequired = exp;
