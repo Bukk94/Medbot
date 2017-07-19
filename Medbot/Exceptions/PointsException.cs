5d4a3632a52c45dd6f7be7ecf27eb2dccc418401
Bukk94 <st46557@student.upce.cz>
2017-07-20 01:30:43 +0200

    Custom exception for points handling
    Custom exception for points handling

---
+++ Medbot/Exceptions/PointsException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Medbot.Exceptions {
+    public class PointsException : Exception {
+
+        /// <summary>
+        /// Initializes a new PointsCommndException instance
+        /// </summary>
+        public PointsException() { }
+
+        /// <summary>
+        /// Initializes a new PointsException instance with specific error message
+        /// </summary>
+        /// <param name="message">Message that describes the error</param>
+        public PointsException(string message) : base(message) { }
+
+    }
+}
