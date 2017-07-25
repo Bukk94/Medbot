97837fd1d516c91494d0ca121d86e888693b1411
Bukk94 <st46557@student.upce.cz>
2017-07-26 01:10:18 +0200

    Events support, property access
    Events support, property access

--- Medbot/IBotClient.cs
+++ Medbot/IBotClient.cs
@@ -1,17 +1,26 @@
-﻿using System;
+﻿using Medbot.Commands;
+using Medbot.Events;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Medbot {
     interface IBotClient {
+        event EventHandler<OnCommandReceivedArgs> OnCommandReceived;
+        event EventHandler<OnMessageArgs> OnMessageReceived;
+        event EventHandler<OnMessageArgs> OnMessageSent;
+        event EventHandler<OnUserArgs> OnUserJoined;
+        event EventHandler<OnUserArgs> OnUserDisconnected;
+
+        List<Command> CommandsList { get; }
+        bool IsConnectionAlive { get; }
+
         void Start();
         void Stop();
         void Connect();
         void Disconnect();
         User GetUserFromChat(string chatLine);
         void SendChatMessage(string msg);
+        void SendChatMessage(string msg, bool isCommand);
         void SendPrivateMessage(string msg, string user);
         void SaveData();
     }
