57820e827a712e0fea4b29c3289c237d5109222d
Bukk94 <st46557@student.upce.cz>
2017-07-26 00:55:23 +0200

    Events support, more commands, updated to C# 6.0
    Events support, more commands, updated to C# 6.0

--- Medbot/MainFrame.Designer.cs
+++ Medbot/MainFrame.Designer.cs
@@ -24,6 +24,7 @@
         /// </summary>
         private void InitializeComponent() {
             this.textBox = new System.Windows.Forms.RichTextBox();
+            this.messageBox = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // textBox
@@ -32,19 +33,30 @@
             this.textBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox.Location = new System.Drawing.Point(12, 12);
             this.textBox.Name = "textBox";
-            this.textBox.Size = new System.Drawing.Size(598, 279);
+            this.textBox.Size = new System.Drawing.Size(547, 279);
             this.textBox.TabIndex = 1;
             this.textBox.Text = "";
+            // 
+            // messageBox
+            // 
+            this.messageBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.messageBox.Location = new System.Drawing.Point(565, 12);
+            this.messageBox.Name = "messageBox";
+            this.messageBox.Size = new System.Drawing.Size(259, 279);
+            this.messageBox.TabIndex = 2;
+            this.messageBox.Text = "";
             // 
             // MainFrame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(622, 303);
+            this.ClientSize = new System.Drawing.Size(836, 303);
+            this.Controls.Add(this.messageBox);
             this.Controls.Add(this.textBox);
             this.Name = "MainFrame";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainFrame_FormClosing);
             this.ResumeLayout(false);
 
         }
@@ -52,6 +64,7 @@
         #endregion
 
         private System.Windows.Forms.RichTextBox textBox;
+        private System.Windows.Forms.RichTextBox messageBox;
     }
 }
 
